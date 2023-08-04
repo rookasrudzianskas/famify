9d70d6f7ba0daf0949f5ee7943aac18e1a629ec7
Rokas <rookas.rudzenskas@gmail.com>
2023-08-04 19:03:23 +0300

    FIX

---
+++ app/terms-of-service.tsx
@@ -0,0 +1,15 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const TermsOfService = () => {
+  return (
+    <View>
+      <Text>
+        byrookas 🚀
+      </Text>
+    </View>
+  );
+};
+
+export default TermsOfService;
