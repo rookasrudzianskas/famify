999350a320f6811b04109a1d244adc9ff0e69192
Rokas <rookas.rudzenskas@gmail.com>
2023-08-09 13:30:46 +0300

    FIX

--- app/goal/[goalId].tsx
+++ app/goal/[goalId].tsx
@@ -1,12 +1,15 @@
 //@ts-nocheck
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {useLocalSearchParams} from "expo-router";
 
 const GoalScreen = () => {
+  const { goalId } = useLocalSearchParams();
+
   return (
     <View>
-      <Text>
-        byrookas 🚀
+      <Text className="text-white">
+        byrookas 🚀 {goalId}
       </Text>
     </View>
   );
