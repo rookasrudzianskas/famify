e82b8a277f717effea6dfdd967710c32d394733b
Rokas <rookas.rudzenskas@gmail.com>
2023-08-10 09:12:02 +0300

    🔧 chore(add-goal.tsx): remove unused imports and update component names for better readability 🔧 chore(package.json): remove unused dependencies and update versions for better compatibility

--- app/(tabs)/add-goal.tsx
+++ app/(tabs)/add-goal.tsx
@@ -1,164 +1,69 @@
-//@ts-nocheck
-import React, {useState} from 'react';
-import {Text, View, StyleSheet, Image, TouchableOpacity, TextInput, ActivityIndicator} from 'react-native';
-import {useRouter} from "expo-router";
-import {EvilIcons, MaterialIcons} from "@expo/vector-icons";
-import * as ImagePicker from 'expo-image-picker';
-import {supabase} from "@/supabase";
-import EmojiSelector, { Categories } from "react-native-emoji-selector";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Link, Tabs } from 'expo-router';
+import {Pressable, useColorScheme, View} from 'react-native';
 
-const OPTIONS = [
-  {
-    id: 0,
-    icon: <EvilIcons name="image" size={24} color="gray" />
-  },
-  {
-    id: 1,
-    icon: <EvilIcons name="camera" size={24} color="gray" />
-  },
-  {
-    id: 2,
-    icon: <EvilIcons name="location" size={24} color="gray" />
-  },
-  {
-    id: 3,
-    icon: <EvilIcons name="spinner-2" size={24} color="gray" />
-  }
-];
+import Colors from '@/constants/Colors';
+import {AntDesign, FontAwesome5, Ionicons, Octicons} from "@expo/vector-icons";
 
-const AddGoal = () => {
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
-  const [content, setContent] = useState<string>('');
-  const [amount, setAmount] = useState<number>(100);
-  const [title, setTitle] = useState<string>('');
-  const [image, setImage] = useState<string | null>(null);
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+  color: string;
+}) {
+  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+}
 
-  async function insertGoal() {
-    try {
-      const { data, error } = await supabase.from('goals').insert({
-        name: title,
-        amount: amount,
-        groupId: 1,
-        emoji: '🤑',
-      });
-
-      if (error) {
-        console.error('Error inserting goal:', error.message);
-        return null;
-      }
-
-      return data;
-    } catch (error) {
-      console.error('Error inserting goal:', error.message);
-      return null;
-    }
-  }
-
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      quality: 0.5,
-    });
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-    }
-  };
-
-  const onSubmit = async () => {
-    const userId = 1;
-    await insertGoal();
-  };
-
-  if(loading) return (
-    <View className="flex-1 items-center justify-center">
-      <ActivityIndicator size="large" color="#0000ff" />
-    </View>
-  );
-
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
 
   return (
-    <View className="flex-1 m-2">
-      <TouchableOpacity activeOpacity={0.8} onPress={() => router.push<any>(`/create-group`)} className="flex flex-row items-center bg-gray-800/70 rounded-lg max-w-[280px] m-5 ml-0 flex-row items-center p-2">
-        <MaterialIcons name="group" size={36} color="white" className="flex items-center justify-center" style={styles.userImage} />
-        <View>
-          <Text className="font-[600] text-white mb-1">Create a group</Text>
-          <Text className="text-[12px] text-gray-600">A Goal of Your Group</Text>
-        </View>
-      </TouchableOpacity>
-
-      <View className="space-y-3">
-        <TextInput
-          placeholder="Name"
-          style={{backgroundColor: '#171616', borderRadius: 4}}
-          className="w-full h-12 px-5 mt-0 text-white border-[1px] border-gray-500 py-1"
-          value={title}
-          onChangeText={setTitle}
-        />
-
-        <TextInput
-          placeholder="1"
-          style={{backgroundColor: '#171616', borderRadius: 4}}
-          className="w-full h-12 px-5 mt-0 text-white border-[1px] border-gray-500 py-1 mb-16"
-          keyboardType="numeric"
-          selectTextOnFocus
-          value={amount.toString()}
-          onChangeText={() => setAmount(amount)}
-        />
-
-        <View className="h-40">
-          <EmojiSelector
-            category={Categories.all}
-            onEmojiSelected={emoji => console.log(emoji)}
-            showSearchBar={false}
-          />
-        </View>
-
-      </View>
-
-      <View className="m-5">
-        {image && <Image source={{ uri: image }} className="w-full h-44 rounded-lg shadow-lg" />}
-      </View>
-
-      <View className="flex flex-row space-x-2 m-5">
-        {OPTIONS.map((option) => (
-          <TouchableOpacity onPress={option.id === 0 ? () => pickImage() : () => {}} key={option.id} activeOpacity={0.8} className="flex bg-gray-900/50 w-10 h-10 items-center justify-center rounded-full">
-            {option.icon}
-          </TouchableOpacity>
-        ))}
-      </View>
-
-      <View className="border-t border-gray-800/60 flex flex-row justify-between">
-        <View/>
-        <View className="flex flex-row items-center justify-center">
-          <View className="px-2 py-1 bg-red-200 rounded-md w-12 flex items-center mr-1 justify-center">
-            <Text className="text-xs text-red-600 font-semibold">NEW</Text>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarLabel: () => null,
+          tabBarIcon: ({ color }) => <AntDesign name="home" size={22} color={color} />,
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="two"
+        options={{
+          title: 'Savings',
+          tabBarLabel: () => null,
+          tabBarIcon: ({ color }) => <FontAwesome5 name="piggy-bank" size={22} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="add-goal"
+        options={{
+          title: 'Add Goal',
+          // hide title
+          tabBarLabel: () => null,
+          tabBarIcon: ({ color }) => <View className="mt-1">
+            <AntDesign name="pluscircle" size={42} color={color} />
           </View>
-          <EvilIcons name="clock" size={24} color="black" />
-          <TouchableOpacity
-            onPress={() => onSubmit()}
-            activeOpacity={0.8} className="flex bg-blue-500 w-28 h-7 items-center justify-center rounded-full m-5">
-            <Text className="text-white">{false ? 'Posting' : 'Add Your Goal'}</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </View>
+        }}
+      />
+      <Tabs.Screen
+        name="transactions/index"
+        options={{
+          title: 'Transactions',
+          tabBarLabel: () => null,
+          tabBarIcon: ({ color }) => <Octicons name="arrow-switch" size={22} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarLabel: () => null,
+          tabBarIcon: ({ color }) => <Ionicons name="settings-outline" size={22} color={color} />,
+        }}
+      />
+    </Tabs>
   );
-};
-
-export default AddGoal;
-
-const styles = StyleSheet.create({
-  userImage: {
-    width: 50,
-
-  },
-  input: {
-    margin: 10,
-    height: 100,
-    textAlignVertical: 'top',
-  }
-});
+}
