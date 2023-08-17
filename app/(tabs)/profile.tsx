9220917d11b924836afe0664796c179793f0ff67
Rokas <rookas.rudzenskas@gmail.com>
2023-08-17 09:47:14 +0300

    🔥 refactor(profile.tsx): remove unnecessary blank line in the code to improve code readability

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -18,7 +18,6 @@ import {useRouter} from "expo-router";
 const blurhash =
   '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
-
 const Profile = () => {
   const {session} = useSupabase();
   const router = useRouter();
