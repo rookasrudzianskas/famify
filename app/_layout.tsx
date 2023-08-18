67899941bdf4ae24c09c13006436aa322d051a2d
Rokas <rookas.rudzenskas@gmail.com>
2023-08-18 17:17:47 +0300

    🚀 feat(_layout.tsx): add onboarding flow to improve user experience and introduce app features 📦 chore(package.json): add react-native-onboard and react-native-onboarding-swiper dependencies for onboarding flow implementation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,13 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
-import {useEffect, useState} from 'react';
-import { useColorScheme } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {useColorScheme, View, Text} from 'react-native';
 import {Session} from "@supabase/supabase-js";
 import {supabase} from "@/supabase";
 import Auth from "@/src/components/auth/auth";
 import { RootSiblingParent } from 'react-native-root-siblings';
+import { OnboardFlow } from 'react-native-onboard';
 
 import {
   QueryClient,
@@ -74,36 +75,47 @@ function RootLayoutNav() {
         <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
 
           <SupabaseProvider>
-            {session && session.user ? (
-              <Stack>
-                <Stack.Screen
-                  name="(tabs)"
-                  options={{ headerShown: false }}
-                />
-                <Stack.Screen
-                  name="modal"
-                  options={{ presentation: 'modal', title: 'Add Transaction' }}
-                />
-                <Stack.Screen
-                  name="account"
-                  options={{ headerShown: false }}
-                />
-                <Stack.Screen
-                  name="goal/[goalId]"
-                  options={{ title: 'Goal Explanation' }}
-                />
-                <Stack.Screen
-                  name="create-group"
-                  options={{ headerShown: false }}
-                />
-                <Stack.Screen
-                  name="group/[groupId]"
-                  options={{ headerShown: false }}
-                />
-              </Stack>
-            ) : (
-              <Auth />
-            )}
+            <OnboardFlow fullscreenModal={true} pages={[
+              {
+                title: 'Welcome to my app',
+                subtitle: 'Cool description of my app',
+                imageUri: 'https://illlustrations.co/static/f8a168f23ea5623d0c8987b551729183/day78-wallet.png',
+              },
+              {
+                title: 'Page 2 header',
+                subtitle: 'Welcome to page 2',
+                imageComponent: <View><Text>Hello!</Text></View>
+              }]} />
+            {/*{session && session.user ? (*/}
+            {/*  <Stack>*/}
+            {/*    <Stack.Screen*/}
+            {/*      name="(tabs)"*/}
+            {/*      options={{ headerShown: false }}*/}
+            {/*    />*/}
+            {/*    <Stack.Screen*/}
+            {/*      name="modal"*/}
+            {/*      options={{ presentation: 'modal', title: 'Add Transaction' }}*/}
+            {/*    />*/}
+            {/*    <Stack.Screen*/}
+            {/*      name="account"*/}
+            {/*      options={{ headerShown: false }}*/}
+            {/*    />*/}
+            {/*    <Stack.Screen*/}
+            {/*      name="goal/[goalId]"*/}
+            {/*      options={{ title: 'Goal Explanation' }}*/}
+            {/*    />*/}
+            {/*    <Stack.Screen*/}
+            {/*      name="create-group"*/}
+            {/*      options={{ headerShown: false }}*/}
+            {/*    />*/}
+            {/*    <Stack.Screen*/}
+            {/*      name="group/[groupId]"*/}
+            {/*      options={{ headerShown: false }}*/}
+            {/*    />*/}
+            {/*  </Stack>*/}
+            {/*) : (*/}
+            {/*  <Auth />*/}
+            {/*)}*/}
           </SupabaseProvider>
         </ThemeProvider>
       </QueryClientProvider>
