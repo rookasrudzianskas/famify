f67d56ead3acc1758aedc282c4da2663201cf19c
Rokas <rookas.rudzenskas@gmail.com>
2023-08-24 09:58:30 +0300

    🔧 fix(NotificationProvider.native.tsx): add missing type declaration for NotificationProviderProps to improve code readability ✨ feat(NotificationProvider.native.tsx): add expoPushToken state and useEffect hook to register for push notifications and handle notification events 📝 docs(register-for-push-async-function.ts): add new file to handle registration for push notifications and get expo push token

--- src/context/NotificationProvider.native.tsx
+++ src/context/NotificationProvider.native.tsx
@@ -1,12 +1,38 @@
-import { createContext } from "react";
+import {createContext, useEffect, useState} from "react";
 
 
 const NotificationContext = createContext({});
 
-export default function NotificationProvider({children}: any) {
-    return (
-        <NotificationContext.Provider value={{}}>
-            {children}
-        </NotificationContext.Provider>
-    )
+export type NotificationProviderProps = {
+  children: React.ReactNode;
 }
+
+export default function NotificationProvider({children}: NotificationProviderProps) {
+  const [expoPushToken, setExpoPushToken] = useState('');
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+
+  return (
+    <NotificationContext.Provider value={{}}>
+      {children}
+    </NotificationContext.Provider>
+  )
+}
+
+
